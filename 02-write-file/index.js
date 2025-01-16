02a2056903f52ec638c430aef58953891637dcb4
MariaPetrovskaia <mariapet70@gmail.com>
2025-01-17 01:17:03 +0200

    feat: complete task2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -5,3 +5,20 @@ const fileName = path.basename("text.txt");
 const fileDir = path.dirname(__filename)
 const filePath = path.join(fileDir, fileName)
 const output = fs.createWriteStream(filePath)
+
+stdout.write("Hello! Please, input your text and press ENTER. To exit input 'exit' of press CTRL + C\n")
+stdin.on("data", (chunk) => {
+  if (chunk.toString().trim() === 'exit') {
+    exit(0)
+  } else {
+    output.write(chunk)
+  }
+})
+process.on("exit", (code) => {
+  if (code === 0) {
+    stdout.write("Thank you! Good luck!");
+  } else {
+    stderr.write(`Something went wrong. The program exited with code ${code}`);
+  }
+});
+process.on('SIGINT', () => exit(0))
